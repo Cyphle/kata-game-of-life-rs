b98489ec4886ddb807f12b6576c29db390da0342
Cyphle <cyril.phamle@gmail.com>
2024-06-30 17:34:34 +0200

    Notes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    // TODO
+    /*
+    Faisons le en objet pour apprendre l'objet avec Rust
+    - Cell with state (alive/dead)
+    - Universe
+     */
     println!("Hello, world!");
 }
